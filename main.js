8b053e078b36cda259fb0e2c50ea97f8c0b5e8c5
jthamayo <jtamayobalogh@gmail.com>
2025-01-02 10:23:09 +0100

    feat: add input to task array

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
-let newTask = document.getElementById("new-task");
-let addTaskForm= document.querySelector(".create-task");
+let newTaskButton = document.getElementById("new-task");
+let taskFormContainer = document.querySelector(".create-task");
+let taskForm = document.getElementById("task-form");
+let tasks = new Array();
 
-newTask.addEventListener("click", ()=>{
-    newTask.style.display = "none";
-    addTaskForm.style.display = "block";
+newTaskButton.addEventListener("click", () => {
+  newTaskButton.style.display = "none";
+  taskFormContainer.style.display = "block";
 });
+
+taskForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  tasks.unshift(recoverData());
+  /*   if (validateInput(taskName)) {
+    console.log("valid");
+  } */
+  addListElement();
+});
+
+function validateInput(name) {}
+
+function recoverData() {
+  let taskName = taskForm.elements.taskName.value.trim();
+  let taskDate = taskForm.elements.taskDate.value.trim();
+  console.log(taskName);
+  console.log(taskDate);
+  return [taskDate, taskName];
+}
+
+function addListElement() {
+  let taskList = document.getElementById("task-list");
+  let newTaskElement = document.createElement("li");
+  newTaskElement.textContent = tasks[0][1];
+  taskList.appendChild(newTaskElement);
+}
